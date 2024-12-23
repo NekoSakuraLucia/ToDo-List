5dba5107a9f967948f7641f7e23d668c18ab6b4f
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-23 15:34:43 +0700

    Added structure for adding Todo and CSS.

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,6 +1,18 @@
+import "./TodoList.css"
+
 function App() {
   return (
-    <div>Main Todo Page</div>
+    <div className="container">
+      <div className="todo-container">
+        <h1>To-Do - List</h1>
+        <input
+          type="text"
+          className="task-input"
+          placeholder="Add a new task..."
+        />
+        <button type="button" className="add-btn">Add</button>
+      </div>
+    </div>
   )
 }
 
