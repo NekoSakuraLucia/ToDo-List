1dec7554b19cd9d3cb391b20b78caf50f1e6c86c
NekoSakuraLucia <mokurolucia@gmail.com>
2025-03-04 23:37:23 +0700

    ติดตั้งโมดูล cors

--- todo-backend/app.js
+++ todo-backend/app.js
@@ -1,13 +1,14 @@
-import { connectDB } from './connection/DBConnect.js';
 import express from 'express';
+import { connectDB } from './connection/DBConnect.js';
+import { router } from './routes/todo.js';
+import cors from 'cors';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 connectDB(process.env.MONGO_URI);
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use(express.json());
+app.use(cors());
+app.use('/todos', router);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
