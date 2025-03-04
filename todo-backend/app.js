cde21a7c2d65404dd0a09423afb2b87c9932f14d
NekoSakuraLucia <mokurolucia@gmail.com>
2025-03-04 21:16:22 +0700

    เพิ่มส่วนการเชื่อมต่อ mongodb

--- todo-backend/app.js
+++ todo-backend/app.js
@@ -1,4 +1,9 @@
+import { connectDB } from './connection/DBConnect.js';
 import express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+connectDB(process.env.MONGO_URI);
 const app = express();
 
 app.get('/', (req, res) => {
