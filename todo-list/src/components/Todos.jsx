9107bba5e681c3872d7df48583d9992aa5211915
NekoSakuraLucia <mokurolucia@gmail.com>
2025-03-05 10:17:08 +0700

    เพิ่มส่วนการทำงานคอมโพเนนต์ TOdos, TodosAdd, ToDosList, และ SearchInput

--- todo-list/src/components/Todos.jsx
+++ todo-list/src/components/Todos.jsx
@@ -1,20 +1,78 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 import SearchInput from './SearchInput';
 import TodosAdd from './TodosAdd';
 import TodosList from './TodosList';
 
 const Todos = () => {
+    const [ToDoData, setToDoData] = useState([]);
+    const [ToDoSearch, setToDoSearch] = useState('');
+    const [ToDoAdd, setToDoAdd] = useState({ title: '' });
+    const [error, setErorr] = useState('');
+    const [filteredToDos, setFilteredTodos] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('/api/todos');
+                if (!response.data) return setErorr('ไม่พบข้อมูล');
+
+                setToDoData(response.data);
+                console.log('log');
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        setFilteredTodos(
+            ToDoData.filter((todo) => {
+                return todo.title
+                    .toLowerCase()
+                    .includes(ToDoSearch.toLowerCase());
+            })
+        );
+    }, [ToDoSearch, ToDoData]);
+
+    const handleSubmit = async () => {
+        if (!ToDoAdd.title) return setErorr('กรุณากรอกข้อมูลให้ครบถ้วน');
+
+        try {
+            const response = await axios.post('/api/todos', {
+                title: ToDoAdd.title,
+            });
+
+            if (!response.data) return setErorr('ไม่สามารถเพิ่มข้อมูลได้');
+            setToDoAdd({ title: '' });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div className="relative min-h-screen flex items-center justify-center text-white">
             <div className="absolute top-[80px] w-full text-center">
                 <h1 className="text-5xl tracking-wider font-bold">ToDo List</h1>
+                <h3 className="mt-4">{error}</h3>
                 <div className="mt-8">
-                    <SearchInput />
+                    <SearchInput
+                        ToDoSearch={ToDoSearch}
+                        setToDoSearch={setToDoSearch}
+                    />
                 </div>
-                <div className="mt-8 space-y-6">
-                    <TodosList />
+                <div className="mt-8 space-y-3">
+                    <TodosList ToDoList={filteredToDos} />
                 </div>
                 <div className="mt-8">
-                    <TodosAdd />
+                    <TodosAdd
+                        handleSubmit={handleSubmit}
+                        ToDoInput={ToDoAdd}
+                        setToDoInput={setToDoAdd}
+                    />
                 </div>
             </div>
         </div>
--- todo-list/src/components/TodosAdd.jsx
+++ todo-list/src/components/TodosAdd.jsx
@@ -1,4 +1,4 @@
-const TodosAdd = () => {
+const TodosAdd = ({ handleSubmit, ToDoInput, setToDoInput }) => {
     return (
         <div className="max-w-[1200px] mx-auto">
             <h1 className="text-2xl tracking-wide font-bold">
@@ -7,11 +7,19 @@ const TodosAdd = () => {
             <div className="mt-2">
                 <input
                     type="text"
+                    onChange={(e) =>
+                        setToDoInput({ ...ToDoInput, title: e.target.value })
+                    }
+                    value={ToDoInput.title}
+                    placeholder="Title"
                     className="bg-purple-400/20 hover:outline hover:outline-1 hover:outline-purple-400 transition-all duration-[250ms] rounded py-2 px-3 w-full max-w-[365px]"
                 />
             </div>
             <div className="mt-8">
-                <button className="bg-white hover:bg-gray-200 transition-colors py-2 px-5 rounded text-black font-bold text-lg">
+                <button
+                    onClick={handleSubmit}
+                    className="bg-white hover:bg-gray-200 transition-colors py-2 px-5 rounded text-black font-bold text-lg"
+                >
                     Submit
                 </button>
             </div>
