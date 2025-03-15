f62fbe4c9741e71eba9368940a98c6aa94af9a55
NekoSakuraLucia <mokurolucia@gmail.com>
2025-03-15 17:49:06 +0700

    เพิ่มการทดสอบ API Todos

---
+++ todo-list/src/test/Todos.test.jsx
@@ -0,0 +1,35 @@
+import '@testing-library/jest-dom';
+import { expect, it, describe, beforeEach, vi } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import Todos from '@/components/Todos';
+import axios from 'axios';
+
+vi.mock('axios');
+
+describe('Todos', () => {
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+
+    it('ตรวจสอบว่าคอมโพเนนต์ render จริงไหม', () => {
+        render(<Todos />);
+        expect(screen.getByText('ToDo List')).toBeInTheDocument();
+    });
+
+    it('ตรวจสอบว่า api ทำงานถูกไหม', async () => {
+        const mockData = [
+            {
+                _id: '67c7428be1f4de2748a509df',
+                title: 'Test 3',
+            },
+        ];
+
+        axios.get.mockResolvedValue({ data: mockData });
+
+        render(<Todos />);
+        await waitFor(() => {
+            expect(axios.get).toHaveBeenCalledWith('/api/todos');
+            expect(screen.getByText('Test 3')).toBeInTheDocument();
+        });
+    });
+});
